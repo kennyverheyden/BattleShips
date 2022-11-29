af78bf05469df1d7661672900b651e57e86e38c6
Kenny Verheyden <kenny.verheyden@gmail.com>
2022-11-29 22:28:56 +0100

    Initial commit

---
+++ src/battleships/Field.java
@@ -0,0 +1,20 @@
+package battleships;
+
+public class Field {
+
+	private String[][]square;
+	int mFieldSize;
+	
+	public Field()
+	{
+		
+	}
+	
+	public String[][] createField(int fieldSize)
+	{
+		 
+	}
+	
+	
+
+}
---
+++ src/battleships/Main.java
@@ -0,0 +1,67 @@
+package battleships;
+
+import java.util.Scanner;
+
+import battleships.Game;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		welcome();
+		Content();
+	}
+
+	public static void EndMSG()
+	{
+		System.out.println("\n  Thank you for playing");
+	}
+
+	public static void Content()
+	{
+		Game game = new Game();
+		Scanner userInput = new Scanner(System.in);
+		boolean exitProgram=false;;
+		boolean validInput= false;
+		int userChose=0;
+
+		do
+		{
+			System.out.println("     1. Play\n");
+			System.out.println("     2. Exit the game\n");
+			do
+			{
+				try
+				{
+					System.out.print("  Choice: ");
+					userChose=userInput.nextInt();
+					validInput=true;
+				}
+				catch(Exception e){
+					userInput.next();
+					System.out.println("Enter valid number\n");
+				}
+			}
+			while(!validInput);
+
+			switch(userChose) {
+			case 1:
+				System.out.println("case1");
+				break;
+			case 2:
+				exitProgram = true;
+				break;
+			}
+		}
+		while(!exitProgram);
+	}
+
+	private static void welcome() {
+		System.out.println("  *************************************************************************");
+		System.out.println("  *****                         Battle Ships                          *****");
+		System.out.println("  *****           Defeat the computer ships in the ocean             *****");
+		System.out.println("  *************************************************************************\n");
+	}
+
+}
